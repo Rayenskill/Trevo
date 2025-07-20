827b4d9a76cb9750d58aedaf99c0cd17349c6b89
SedELB <saadoun500@gmail.com>
2025-07-20 13:24:50 -0400

    msg not working

--- loginpage/loginapp/views.py
+++ loginpage/loginapp/views.py
@@ -4,10 +4,25 @@ from django.contrib import messages
 
 # Create your views here.
 def login_user(request):
-    if request.method == 'POST': # arriving on the page transporting info (after filling)(POST)
-        pass
-    else: # arriving on the page without transporting info (GET)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password) # built-in contrib.auth
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return redirect('login')
+    else:
         return render(request, "registration/login.html", {})
+
+
+    # if request.method == 'POST': # arriving on the page transporting info (after filling)(POST)
+    #     username = request.POST['username']
+    #     pass
+    # else: # arriving on the page without transporting info (GET)
+    #     return render(request, "registration/login.html", {})
 
 
 
