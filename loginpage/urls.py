9ab968618d18027593d31baac452e4431c75e989
SedELB <saadoun500@gmail.com>
2025-07-22 18:55:01 -0400

    user creation working

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="login")
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name='register'),
 ]
 
 # commit
