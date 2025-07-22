fc4bb6c9be7999d0bb0b4b9f2fe1be3cbbe7586d
Rayenskill <raybour9@gmail.com>
2025-07-22 19:45:40 -0400

    Problème technique

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', include('homepage.urls')),
+    path("home/", include('homepage.urls')),
+    path('admin/', admin.site.urls),
     path('login/', views.login_view, name="login"),
     path('register/', views.register_view, name='register'),
 ]
