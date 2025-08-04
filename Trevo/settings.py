70c7c561af9f39172a00fdef9af83a64aef8b133
Rayenskill <raybour9@gmail.com>
2025-08-03 21:36:57 -0400

    quelques updates

--- Trevo/settings.py
+++ Trevo/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = ''
-STATICFILES_DIRS = ('static',)
+STATICFILES_DIRS = ('static',"../homepage/static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
