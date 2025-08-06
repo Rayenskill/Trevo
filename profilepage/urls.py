6009e33e61a21ee04f4fdacbbca5c9e1c989d5db
Rayenskill <raybour9@gmail.com>
2025-08-05 20:21:56 -0400

    début du front end du profile page et de la page de listing

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from views import profilepage
+from . import views
 
 urlpatterns = [
-    path("", profilepage, name="profilepage"),
+    path("", views.profilepage, name="profilepage"),
 ]
